61882d0aadf768e50643c9b3bcea7ed2368bed9a
Brent <burentoperii@gmail.com>
2016-07-02 14:45:27 -0500

    Initial commit of NameAge

---
+++ NameAge/Source.cpp
@@ -0,0 +1,10 @@
+#include "../../../../std_lib_facilities.h"
+
+int main()
+{
+	cout << "Please enter your name and age: \n";
+	string name;
+	int age;
+	cin >> name >> age;
+	cout << "Hello, " << name << " (age " << age << ")\n";
+}
